99455e1d17347ee7bce576dfcb8039c9e1d22c40
https://github.com/zhouytao/tomz.git <zhouytao@yeah.net>
2013-10-19 13:19:11 +0800

    项目结构重构
    将静态资源放到static目录中，将不用的视图删除

--- tomz.go
+++ tomz.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-	"github.com/astaxie/beego"
-	"tomz/controllers"
+	// "github.com/astaxie/beego"
+	// "tomz/controllers"
+	"html/template"
+	"log"
+	"net/http"
 )
 
 func main() {
-	beego.Router("/", &controllers.MainController{})
-	beego.Router("/edit", &controllers.EditController{})
-	beego.Run()
+	// beego.Router("/", &controllers.MainController{})
+	// beego.Router("/edit", &controllers.EditController{})
+	// beego.Run()
+
+	http.HandleFunc("/", indexHandle)
+	err := http.ListenAndServe(8080, handler)
+	log.Fatal(err)
+}
+
+func indexHandle(rw http.ResponseWriter, req *http.Request) {
+	t, err := template.ParseFiles("index.html", "footer.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Execute(rw, nil)
 }
